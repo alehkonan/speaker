d0143f69e1ef3838ab73a887a888b07907db00fa
Aleh <olegkonanff@gmail.com>
2025-02-09 14:03:38 +0400

    feat: :lipstick: add button and button link components

---
+++ src/components/Button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { HTMLButtonAttributes } from 'svelte/elements';
+
+  let { children, ...props }: Omit<HTMLButtonAttributes, 'class'> = $props();
+</script>
+
+<button
+  {...props}
+  class="bg-ripe-mango cursor-pointer rounded-xl p-1 font-semibold text-white"
+>
+  {@render children?.()}
+</button>
---
+++ src/components/ButtonLink.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { HTMLAnchorAttributes } from 'svelte/elements';
+
+  let { children, ...props }: Omit<HTMLAnchorAttributes, 'class'> = $props();
+</script>
+
+<a
+  {...props}
+  class="bg-ripe-mango cursor-pointer rounded-xl p-1 text-center font-semibold text-white"
+>
+  {@render children?.()}
+</a>
